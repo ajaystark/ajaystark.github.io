d7d78b9be530fee452841d1f50ef4db0dcb1e8e8
Ajay Prakash <ajay18215@iiitd.ac.in>
2019-07-11 22:27:57 +0530

    added resume and unicorn

---
+++ script.js
@@ -0,0 +1,11 @@
+function on() {
+  document.getElementById("overlay").style.display = "block";
+  document.body.scroll="no";
+  document.body.style.overflow="hidden";
+}
+
+function off() {
+  document.getElementById("overlay").style.display = "none";
+  document.body.scroll="yes";
+  document.body.style.overflow="visible";
+}
