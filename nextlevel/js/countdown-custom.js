a4a291918749d501e60ee4efe9fc01385de43c32
Ajay Prakash <ajay.prakash@cactusglobal.com>
2022-08-18 14:17:36 +0530

    done

--- nextlevel/js/countdown-custom.js
+++ nextlevel/js/countdown-custom.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function() {
         $(function () {
-            $('#defaultCountdown').countdown({until: new Date(2022  , 09, 10, 0)}); // year, month, date, hour
+            $('#defaultCountdown').countdown({until: new Date(2022  , 08, 2, 0)}); // year, month, date, hour
         });
 });		
 
