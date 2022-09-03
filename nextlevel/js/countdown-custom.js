93865595beb462657df1328623f9b91419827349
ajayp-cactus <ajay.prakash@cactusglobal.com>
2022-09-04 01:00:32 +0530

    countdown updated

--- nextlevel/js/countdown-custom.js
+++ nextlevel/js/countdown-custom.js
@@ -1,6 +1,31 @@
 jQuery(document).ready(function() {
         $(function () {
-            $('#defaultCountdown').countdown({until: new Date(2022  , 08, 5, 10)}); // year, month, date, hour
+            var array=[5,6,9];
+
+            var map_json={};
+            for(const i of array){
+                map_json[i] = new Date(2022,8,i,10);
+            }
+            var currdate=new Date();
+
+            // console.log(map_json);
+            console.log(currdate);
+            // console.log(map_json[currdate.getDate()]);
+            // $('#defaultCountdown').countdown({until:map_json[currdate.getDate()]});
+
+            var a=currdate.getDate();
+            if(a<6){
+                $('#defaultCountdown').countdown({until:map_json[5]});
+                $('#main-title').html("Lovely Professional <br> University ,");
+            }
+            else if(a<7){
+                $('#defaultCountdown').countdown({until:map_json[6]});
+                $('#main-title').html("CHANDIGARH University <br> (Chitkara),");
+            }
+            else if(a<10){
+                $('#defaultCountdown').countdown({until:map_json[9]});
+                $('#main-title').html("Amity University ,<br>");
+            }
         });
 });		
 
