01325074d6ccc932c737b4b6dd407b08fe1d6f47
ajayp-cactus <ajay.prakash@cactusglobal.com>
2023-01-05 13:37:33 +0530

    we socilaise u

--- nextlevel/js/countdown-custom.js
+++ nextlevel/js/countdown-custom.js
@@ -1,30 +1,27 @@
 jQuery(document).ready(function() {
         $(function () {
-            var array=[5,6,9];
+            var array=[1,2,7];
 
             var map_json={};
             for(const i of array){
-                map_json[i] = new Date(2022,8,i,10);
+                map_json[i] = new Date(2022,11,i,10);
             }
             var currdate=new Date();
 
-            // console.log(map_json);
-            console.log(currdate);
-            // console.log(map_json[currdate.getDate()]);
-            // $('#defaultCountdown').countdown({until:map_json[currdate.getDate()]});
-
             var a=currdate.getDate();
-            if(a<6){
-                $('#defaultCountdown').countdown({until:map_json[5]});
-                $('#main-title').html("Lovely Professional <br> University ,");
+            console.log(map_json[1]);
+
+            if(a<1){
+                $('#defaultCountdown').countdown({until:map_json[1]});
+                $('#main-title').html("INTEGRAL UNIVERSITY  <br> <span class='id-color' style='color:var(--secondary-color) !important'>Lucknow</span>");
             }
-            else if(a<7){
-                $('#defaultCountdown').countdown({until:map_json[6]});
-                $('#main-title').html("CHANDIGARH University <br> (Chitkara),");
+            else if(a<2){
+                $('#defaultCountdown').countdown({until:map_json[2]});
+                $('#main-title').html("Bennett University <br> <span class='id-color' style='color:var(--secondary-color) !important'>Greater Noida</span>");
             }
-            else if(a<10){
-                $('#defaultCountdown').countdown({until:map_json[9]});
-                $('#main-title').html("Amity University ,<br>");
+            else{
+                $('#defaultCountdown').countdown({until:map_json[7]});
+                $('#main-title').html("Delhi Technological  <br> University <br> <span class='id-color' style='color:var(--secondary-color) !important'>New Delhi</span>");
             }
         });
 });		
